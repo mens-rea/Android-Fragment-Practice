d3a7f477b3b3d58b57f6a62faf2f5e1d9ab4975b
Juan Miguel Alvarez <juan.miguel.g.alvarez@gmail.com>
2017-12-08 10:23:58 +0800

    created bottom navigation

--- app/src/main/java/com/fragment/juanshotman/fragmentpractice/MainActivity.java
+++ app/src/main/java/com/fragment/juanshotman/fragmentpractice/MainActivity.java
@@ -3,16 +3,44 @@ package com.fragment.juanshotman.fragmentpractice;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private FragmentStateAdapter fragmentStateAdapter;
     private ViewPager viewPager;
+    private Button homeButton, aboutButton, servicesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        homeButton = findViewById(R.id.home_button);
+        aboutButton = findViewById(R.id.about_button);
+        servicesButton = findViewById(R.id.services_button);
+
+        homeButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                setActiveFragment(0);
+            }
+        });
+
+        aboutButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                setActiveFragment(1);
+            }
+        });
+
+        servicesButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                setActiveFragment(2);
+            }
+        });
 
         fragmentStateAdapter = new FragmentStateAdapter(getSupportFragmentManager());
         viewPager = findViewById(R.id.view_pager);
--- app/src/main/java/com/fragment/juanshotman/fragmentpractice/ServicesFragment.java
+++ app/src/main/java/com/fragment/juanshotman/fragmentpractice/ServicesFragment.java
@@ -10,37 +10,10 @@ import android.widget.Button;
 
 public class ServicesFragment extends Fragment{
 
-    private Button homeButton, aboutButton, servicesButton;
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_services, container, false);
-
-        homeButton = view.findViewById(R.id.home_button);
-        aboutButton = view.findViewById(R.id.about_button);
-        servicesButton = view.findViewById(R.id.services_button);
-
-        homeButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                ((MainActivity)getActivity()).setActiveFragment(0);
-            }
-        });
-
-        aboutButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                ((MainActivity)getActivity()).setActiveFragment(1);
-            }
-        });
-
-        servicesButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                ((MainActivity)getActivity()).setActiveFragment(2);
-            }
-        });
 
         return view;
     }
